5a2fa8a628fe5e57d381fcfd88d1b5314426d7ef
misaf <misaf.1990@gmail.com>
2026-07-11 20:47:45 +0000

    wip

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use LaravelJsonApi\Laravel\Facades\JsonApiRoute;
 use LaravelJsonApi\Laravel\Http\Controllers\JsonApiController;
 use LaravelJsonApi\Laravel\Routing\ResourceRegistrar;
 
-Route::middleware('api')->group(function (): void {
+Route::middleware(['api', 'vendra.locale'])->group(function (): void {
     JsonApiRoute::server('vendra-attribute')->prefix('v1')->resources(function (ResourceRegistrar $server): void {
         $server->resource('attribute-values', JsonApiController::class)
             ->readOnly();
